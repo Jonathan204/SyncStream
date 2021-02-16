b1d3039f0a5380df668ab153b24c4150842214cd
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2021-02-16 14:35:34 -0600

    42 Create Users Schema (#51)
    * create env example and update package-lock
    
    * Change posts to users and support .env
    
    - Replace everything related to posts
    - Support .env to secure our databse

--- server/index.js
+++ server/index.js
@@ -1,21 +1,26 @@
+import dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
+const config = dotenv.config()
+if (config.error){
+  throw config.error;
+}
+const env = config.parsed;
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/posts", postRoutes); //every route inside of post routes will start with post
+app.use("/users", userRoutes); //every route inside of post routes will start with post
 
-const CONNECTION_URL =
-  "mongodb+srv://Jonathan:Jonathan123@cluster0.2rpdj.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = env.DB_CONNECTION;
+const PORT = env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
