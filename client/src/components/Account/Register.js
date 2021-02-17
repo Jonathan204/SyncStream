ba7d8b092581420d12ae8fedc3ce23e144b9bcb7
mjbathtub <mbathie1999@gmail.com>
2021-02-16 22:03:23 -0600

    Basic layout for register page

--- client/src/components/Account/Register.js
+++ client/src/components/Account/Register.js
@@ -1,9 +1,32 @@
-import React, { Component } from 'react';
-import { Nav, Navbar, Image } from 'react-bootstrap';
+import React from 'react';
+import { Form, Container, Button } from 'react-bootstrap';
 
 function Register() {
     return (
-        <div></div>
+        <Container>
+            <h3>Please Sign Up!</h3>
+            <Form>
+                <Form.Group conrolId="email">
+                    <Form.Control type="email" placeholder="Email" />
+                </Form.Group>
+
+                <Form.Group controlId="username">
+                    <Form.Control placeholder="Username" />
+                </Form.Group>
+
+                <Form.Group controlId="password">
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group controlId="confirmPassword">
+                    <Form.Control type="password" placeholder="Confirm Password" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Register
+                </Button>
+            </Form>
+        </Container>
     );
 }
 
