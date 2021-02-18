c5514e56fc5fd0fba225db51cf5a0fda26b30966
Jonathan Kuz <43478694+Jonathan204@users.noreply.github.com>
2021-02-18 16:23:21 -0600

    add Spotify APi
    adds the spotify api to display in the infobox, but it cant handle ads

---
+++ client/src/components/Map/infoWindow/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "bebeb728633e467d92f913fc90610698";
+export const redirectUri = "http://localhost:3000/";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
--- client/src/components/Map/infoWindow/currLocation.js
+++ client/src/components/Map/infoWindow/currLocation.js
@@ -1,23 +1,100 @@
 import React from 'react'
-import { Image, Container, Row, Column } from 'react-bootstrap';
+import { Image, Container, Row } from 'react-bootstrap';
 import profilePicture from "../../../images/default_account.png";
-
-const InfoWindow = (props) => {
+import * as $ from "jquery";
+import { authEndpoint, clientId, redirectUri, scopes } from "./config";
+import Player from "../../Player/Player";
+import hash from "./hash";
+class InfoWindow extends React.Component{
 
   //im honestly forgot we were using bootstrap at this point and just customly made the styling
   //we can change it later if we want, or right now just lemme know in the pr.
-  const infoWindowStyle = {
-    position: 'relative',
-    bottom: 200,
-    left: '-45px',
-    width: 220,
-    backgroundColor: 'white',
-    boxShadow: '0 2px 7px 1px rgba(0, 0, 0, 0.3)',
-    padding: 10,
-    fontSize: 14,
-    zIndex: 100,
-  };
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+      item: {
+        album: {
+          images: [{ url: "" }]
+        },
+        name: "",
+        artists: [{ name: "" }],
+        duration_ms: 0
+      },
+      is_playing: "Paused",
+      progress_ms: 0,
+      no_data: false,
+    };
 
+    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+    this.tick = this.tick.bind(this);
+  }
+
+  componentDidMount() {
+    // Set token
+    let _token = hash.access_token;
+
+    if (_token) {
+      // Set token
+      this.setState({
+        token: _token
+      });
+      this.getCurrentlyPlaying(_token);
+    }
+
+    // set interval for polling every 5 seconds
+    this.interval = setInterval(() => this.tick(), 5000);
+  }
+  componentWillUnmount() {
+    // clear the interval to save resources
+    clearInterval(this.interval);
+  }
+
+  tick() {
+    if(this.state.token) {
+      this.getCurrentlyPlaying(this.state.token);
+    }
+  }
+
+  getCurrentlyPlaying(token) {
+    // Make a call using the token
+    $.ajax({
+      url: "https://api.spotify.com/v1/me/player",
+      type: "GET",
+      beforeSend: xhr => {
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+      },
+      success: data => {
+        // Checks if the data is not empty
+        if(!data) {
+          this.setState({
+            no_data: true,
+          });
+          return;
+        }
+
+        this.setState({
+          item: data.item,
+          is_playing: data.is_playing,
+          progress_ms: data.progress_ms,
+          no_data: false /* We need to "reset" the boolean, in case the
+                            user does not give F5 and has opened his Spotify. */
+        });
+      }
+    });
+  }
+render(){
+    const infoWindowStyle = {
+        position: 'relative',
+        bottom: 200,
+        left: '-45px',
+        width: 220,
+        backgroundColor: 'white',
+        boxShadow: '0 2px 7px 1px rgba(0, 0, 0, 0.3)',
+        padding: 10,
+        fontSize: 14,
+        zIndex: 100,
+      };
   return (
     <div style={infoWindowStyle}>
       <Container>
@@ -32,10 +109,34 @@ const InfoWindow = (props) => {
             roundedCircle
           />
         </Row>
+        <Row>
+        {!this.state.token && (
+            <a
+              className="btn btn--loginApp-link"
+              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                "%20"
+              )}&response_type=token&show_dialog=true`}
+            >
+              Login to Spotify
+            </a>
+          )}
+          {this.state.token && !this.state.no_data && (
+            <Player
+              item={this.state.item}
+              is_playing={this.state.is_playing}
+              progress_ms={this.state.progress_ms}
+            />
+          )}
+          {this.state.no_data && (
+            <p>
+              You need to be playing a song on Spotify, for something to appear here.
+            </p>
+          )}    
+        </Row>
       </Container>
 
     </div>
-  );
+  )}
 };
 
 export default InfoWindow;
