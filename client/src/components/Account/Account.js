534be58a3ea0c6b932e46ebfddd954b3b64e9bcf
mjbathtub <mbathie1999@gmail.com>
2021-02-20 20:11:43 -0600

    Prettier files

--- client/src/components/Account/Account.js
+++ client/src/components/Account/Account.js
@@ -1,43 +1,41 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Login from './Login.js';
-import { Container } from 'react-bootstrap';
-import { AccountContext } from './AccountContext.js';
-import Register from './Register.js';
+import Login from "./Login.js";
+import { Container } from "react-bootstrap";
+import { AccountContext } from "./AccountContext.js";
+import Register from "./Register.js";
 
 const Account = () => {
-    const [activeWindow, setActiveWindow] = useState("signin");
+  const [activeWindow, setActiveWindow] = useState("signin");
 
-    const switchToSignup = () => {
-        setActiveWindow("signup")
-    }
+  const switchToSignup = () => {
+    setActiveWindow("signup");
+  };
 
-    const switchToSignin = () => {
-        setActiveWindow("signin")
-    }
+  const switchToSignin = () => {
+    setActiveWindow("signin");
+  };
 
-    const accountWindow = {
-        height: 550,
-        width: 300,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        borderRadius: 20,
-        boxShadow: '0 2px 7px 1px rgba(0, 0, 0, 0.3)',
-        marginTop: '250px'
-      };
+  const accountWindow = {
+    height: 550,
+    width: 300,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    borderRadius: 20,
+    boxShadow: "0 2px 7px 1px rgba(0, 0, 0, 0.3)",
+    marginTop: "250px",
+  };
 
-    const contextValue = {switchToSignup, switchToSignin};
-    return (
-        <AccountContext.Provider value={contextValue}>
-        <Container className="border" style={accountWindow}>
-
-                {activeWindow === "signin" && <Login />}
-                {activeWindow === "signup" && <Register />}
-
-        </Container>
-        </AccountContext.Provider>
-    );
-}
+  const contextValue = { switchToSignup, switchToSignin };
+  return (
+    <AccountContext.Provider value={contextValue}>
+      <Container className="border" style={accountWindow}>
+        {activeWindow === "signin" && <Login />}
+        {activeWindow === "signup" && <Register />}
+      </Container>
+    </AccountContext.Provider>
+  );
+};
 
 export default Account;
