fc3ca66286da288d3bfc36267b77f09165170341
ColeVoelpel <66435041+ColeVoelpel@users.noreply.github.com>
2021-02-20 20:54:24 -0600

    Fixed browser console errors

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { Container } from "react-bootstrap";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //redux dependencies
 import { Provider } from "react-redux"; //keeps track of the store which is the global state which allows us to access the store from anywhere within the app.
@@ -14,16 +14,19 @@ import reducers from "./reducers";
 import App from "./App";
 import Account from "./components/Account/Account";
 
-const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
       <Switch>
         <Route exact path="/">
-        <Container>
-          <Account />
-        </Container>
+          <Container>
+            <Account />
+          </Container>
         </Route>
         <Route path="/home">
           <App />
