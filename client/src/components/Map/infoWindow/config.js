63cce1aea608d833712b723e666af455606d2a03
Jonathan Kuz <43478694+Jonathan204@users.noreply.github.com>
2021-02-24 17:24:47 -0600

    add token setters
    Add setter and getter for access token

--- client/src/components/Map/infoWindow/config.js
+++ client/src/components/Map/infoWindow/config.js
@@ -8,3 +8,19 @@ export const scopes = [
     "user-read-currently-playing",
     "user-read-playback-state",
 ];
+
+export function getAccessToken(){
+    var expires = 0 + localStorage.getItem('pa_expires', '0');
+    if ((new Date()).getTime() > expires) {
+        return '';
+    }
+    var token = localStorage.getItem('pa_token', '');
+    return token;
+}
+
+export function setAccessToken(token, expires_in){
+    localStorage.setItem('pa_token', token);
+    localStorage.setItem('pa_expires', (new Date()).getTime() + expires_in);
+    // _token = token;
+    // _expires = expires_in;
+}
