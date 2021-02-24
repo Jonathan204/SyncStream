6ebafa571c0fd7c35f14ef9a26919af9625f6aa2
ColeVoelpel <66435041+ColeVoelpel@users.noreply.github.com>
2021-02-24 11:49:53 -0600

    Removed improperly merged code

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import React from "react";
-import { AppBar, Grow, Grid } from "@material-ui/core";
 import "bootstrap/dist/css/bootstrap.min.css"; //This is required for react bootstrap CSS styling
 import Map from "./components/Map/Map";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
