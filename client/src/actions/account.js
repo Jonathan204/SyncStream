21b9a568e103f0ac6260e3603a5becec5b144314
mjbathtub <mbathie1999@gmail.com>
2021-02-24 19:12:12 -0600

    Revert "Redirected after registering"
    This reverts commit 1935c076a43747e895d70d0d1935a6f0aa91ff2d.

--- client/src/actions/account.js
+++ client/src/actions/account.js
@@ -1,51 +1,50 @@
 import {
-	CREATE_SUCCESS,
-	CREATE_ERROR,
-	LOGIN,
-	LOGIN_ERROR,
-	UPDATE_USER,
-	LOADING,
+  CREATE_SUCCESS,
+  CREATE_ERROR,
+  LOGIN,
+  LOGIN_ERROR,
+  UPDATE_USER,
+  LOADING,
 } from "../constants/actionTypes";
 
 import * as api from "../api/index.js";
 
-export const createUser = (user, history) => async (dispatch) => {
-	try {
-		dispatch({ type: LOADING, payload: true });
-		const { data } = await api.createUser(user);
-		dispatch({ type: CREATE_SUCCESS, payload: handleResponse(data).message });
-		history.push("/home");
-	} catch (error) {
-		const { message } = handleError(error);
-		dispatch({ type: CREATE_ERROR, payload: message });
-	}
+export const createUser = (user) => async (dispatch) => {
+  try {
+    dispatch({ type: LOADING, payload: true });
+    const { data } = await api.createUser(user);
+    dispatch({ type: CREATE_SUCCESS, payload: handleResponse(data).message });
+  } catch (error) {
+    const { message } = handleError(error);
+    dispatch({ type: CREATE_ERROR, payload: message });
+  }
 };
 
 export const loginUser = (user, history) => async (dispatch) => {
-	try {
-		dispatch({ type: LOADING, payload: true });
-		const { data } = await api.login(user);
-		dispatch({ type: LOGIN, payload: handleResponse(data) });
-		history.push("/home");
-	} catch (error) {
-		const { message } = handleError(error);
-		dispatch({ type: LOGIN_ERROR, payload: message });
-	}
+  try {
+    dispatch({ type: LOADING, payload: true });
+    const { data } = await api.login(user);
+    dispatch({ type: LOGIN, payload: handleResponse(data) });
+    history.push("/home");
+  } catch (error) {
+    const { message } = handleError(error);
+    dispatch({ type: LOGIN_ERROR, payload: message });
+  }
 };
 
 export const updateUser = (id, user) => async (dispatch) => {
-	try {
-		const { data } = await api.updateUser(id, user);
+  try {
+    const { data } = await api.updateUser(id, user);
 
-		dispatch({ type: UPDATE_USER, payload: data });
-	} catch (error) {
-		console.log(error.message);
-	}
+    dispatch({ type: UPDATE_USER, payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 function handleError(error) {
-	return error.response && error.response.data ? error.response.data : error;
+  return error.response && error.response.data ? error.response.data : error;
 }
 function handleResponse(resp) {
-	return resp.data ? resp.data : resp;
+  return resp.data ? resp.data : resp;
 }
--- client/src/components/Account/Register.js
+++ client/src/components/Account/Register.js
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from "react";
 import { Form, Container, Row, Col, Alert } from "react-bootstrap";
-import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { AccountContext } from "./AccountContext";
 import { createUser } from "../../actions/account";
@@ -8,137 +7,136 @@ import LoaderButton from "../Button/LoadingButton";
 import validate from "./validation";
 
 const Register = () => {
-	const [validated, setValidated] = useState(false);
-	const [userData, setUserData] = useState({
-		username: "",
-		email: "",
-		password: "",
-		confirmPassword: "",
-	});
-	const [errors, setErrors] = useState({});
-	const userError = useSelector((state) => state.account.createError);
-	const userCreated = useSelector((state) => state.account.createMessage);
-	const loading = useSelector((state) => state.account.loading);
-	const { switchToSignin } = useContext(AccountContext);
-	const history = useHistory();
-	const dispatch = useDispatch();
+  const [validated, setValidated] = useState(false);
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [errors, setErrors] = useState({});
+  const userError = useSelector((state) => state.account.createError);
+  const userCreated = useSelector((state) => state.account.createMessage);
+  const loading = useSelector((state) => state.account.loading);
+  const { switchToSignin } = useContext(AccountContext);
+  const dispatch = useDispatch();
 
-	const handleSubmit = (event) => {
-		setValidated(true);
-		event.preventDefault();
-		const formErrors = validate(userData);
-		if (Object.keys(formErrors).length > 0) {
-			setErrors(formErrors);
-			event.stopPropagation();
-		} else {
-			setErrors({});
-			dispatch(createUser(userData, history));
-		}
-	};
+  const handleSubmit = (event) => {
+    setValidated(true);
+    event.preventDefault();
+    const formErrors = validate(userData);
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
+      event.stopPropagation();
+    } else {
+      setErrors({});
+      dispatch(createUser(userData));
+    }
+  };
 
-	return (
-		<Container className="account-height">
-			<Row className="mt-4 align-text-center">
-				<Col>
-					<h3>Please Sign Up!</h3>
-				</Col>
-			</Row>
-			<Form className="mt-5" noValidate onSubmit={handleSubmit}>
-				<Row className="flex-column">
-					<Form.Group controlId="email">
-						<Form.Control
-							name="email"
-							type="email"
-							placeholder="Email"
-							required
-							isValid={validated && !errors.email}
-							isInvalid={!!errors.email || !!userError}
-							onChange={(e) =>
-								setUserData({ ...userData, email: e.target.value })
-							}
-						/>
-						<Form.Control.Feedback type="invalid">
-							{errors.email}
-						</Form.Control.Feedback>
-					</Form.Group>
-				</Row>
-				<Row className="flex-column">
-					<Form.Group controlId="username">
-						<Form.Control
-							name="username"
-							required
-							type="text"
-							placeholder="Username"
-							isValid={validated && !errors.username}
-							isInvalid={!!errors.username || !!userError}
-							onChange={(e) =>
-								setUserData({ ...userData, username: e.target.value })
-							}
-						/>
-						<Form.Control.Feedback type="invalid">
-							{errors.username}
-						</Form.Control.Feedback>
-					</Form.Group>
-				</Row>
-				<Row className="flex-column">
-					<Form.Group controlId="password">
-						<Form.Control
-							name="password"
-							required
-							type="password"
-							placeholder="Password"
-							isValid={validated && !errors.password}
-							isInvalid={!!errors.password || !!userError}
-							onChange={(e) =>
-								setUserData({ ...userData, password: e.target.value })
-							}
-						/>
-						<Form.Control.Feedback type="invalid">
-							{errors.password}
-						</Form.Control.Feedback>
-					</Form.Group>
-				</Row>
-				<Row className="flex-column">
-					<Form.Group controlId="confirmPassword">
-						<Form.Control
-							name="password"
-							required
-							type="password"
-							placeholder="Confirm Password"
-							isValid={validated && !errors.confirmPassword}
-							isInvalid={!!errors.confirmPassword || !!userError}
-							onChange={(e) =>
-								setUserData({ ...userData, confirmPassword: e.target.value })
-							}
-						/>
-						<Form.Control.Feedback type="invalid">
-							{errors.confirmPassword}
-						</Form.Control.Feedback>
-					</Form.Group>
-				</Row>
-				{(userError || userCreated) && (
-					<Alert variant={userError ? "danger" : "success"}>
-						{userError || userCreated}
-					</Alert>
-				)}
+  return (
+    <Container className="account-height">
+      <Row className="mt-4 align-text-center">
+        <Col>
+          <h3>Please Sign Up!</h3>
+        </Col>
+      </Row>
+      <Form className="mt-5" noValidate onSubmit={handleSubmit}>
+        <Row className="flex-column">
+          <Form.Group controlId="email">
+            <Form.Control
+              name="email"
+              type="email"
+              placeholder="Email"
+              required
+              isValid={validated && !errors.email}
+              isInvalid={!!errors.email || !!userError}
+              onChange={(e) =>
+                setUserData({ ...userData, email: e.target.value })
+              }
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.email}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row className="flex-column">
+          <Form.Group controlId="username">
+            <Form.Control
+              name="username"
+              required
+              type="text"
+              placeholder="Username"
+              isValid={validated && !errors.username}
+              isInvalid={!!errors.username || !!userError}
+              onChange={(e) =>
+                setUserData({ ...userData, username: e.target.value })
+              }
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.username}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row className="flex-column">
+          <Form.Group controlId="password">
+            <Form.Control
+              name="password"
+              required
+              type="password"
+              placeholder="Password"
+              isValid={validated && !errors.password}
+              isInvalid={!!errors.password || !!userError}
+              onChange={(e) =>
+                setUserData({ ...userData, password: e.target.value })
+              }
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.password}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row className="flex-column">
+          <Form.Group controlId="confirmPassword">
+            <Form.Control
+              name="password"
+              required
+              type="password"
+              placeholder="Confirm Password"
+              isValid={validated && !errors.confirmPassword}
+              isInvalid={!!errors.confirmPassword || !!userError}
+              onChange={(e) =>
+                setUserData({ ...userData, confirmPassword: e.target.value })
+              }
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.confirmPassword}
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        {(userError || userCreated) && (
+          <Alert variant={userError ? "danger" : "success"}>
+            {userError || userCreated}
+          </Alert>
+        )}
 
-				<Row className="mt-5">
-					<LoaderButton
-						className="submit-button"
-						type="submit"
-						loading={loading}
-					>
-						Register
-					</LoaderButton>
-				</Row>
-				<Row className="flex-column mt-3 align-text-center">
-					<p className="underline-on-hover" onClick={switchToSignin}>
-						Already have an account? Login!
-					</p>
-				</Row>
-			</Form>
-		</Container>
-	);
+        <Row className="mt-5">
+          <LoaderButton
+            className="submit-button"
+            type="submit"
+            loading={loading}
+          >
+            Register
+          </LoaderButton>
+        </Row>
+        <Row className="flex-column mt-3 align-text-center">
+          <p className="underline-on-hover" onClick={switchToSignin}>
+            Already have an account? Login!
+          </p>
+        </Row>
+      </Form>
+    </Container>
+  );
 };
 
 export default Register;
