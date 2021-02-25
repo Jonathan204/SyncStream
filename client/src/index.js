f1d4911d35c39614c1f4e55cc3f2d0099ec144a3
mjbathtub <mbathie1999@gmail.com>
2021-02-25 00:50:01 -0600

    Added redux persist + fixed profile refresh bug

--- client/src/index.js
+++ client/src/index.js
@@ -4,19 +4,35 @@ import { composeWithDevTools } from "redux-devtools-extension";
 
 //redux dependencies
 import { Provider } from "react-redux"; //keeps track of the store which is the global state which allows us to access the store from anywhere within the app.
+import { persistStore, persistReducer } from "redux-persist";
 import { createStore, applyMiddleware } from "redux";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import thunk from "redux-thunk";
 import reducers from "./reducers";
 import App from "./App";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
+};
+
+const pReducer = persistReducer(persistConfig, reducers);
 
 const store = createStore(
-  reducers,
+  pReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
