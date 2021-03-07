a5fb3d5fc14059e69c2447655f0a54c0b4a2b05a
Chris Melendez <chris.melendezg@gmail.com>
2021-03-06 20:10:02 -0600

    Add temp .env to backend ci

--- server/index.js
+++ server/index.js
@@ -7,11 +7,7 @@ import cors from "cors";
 import userRoutes from "./routes/users.js";
 
 const app = express();
-const config = dotenv.config();
-if (config.error) {
-  throw config.error;
-}
-const env = config.parsed;
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -19,8 +15,8 @@ app.use(cors());
 
 app.use("/users", userRoutes); //every route inside of post routes will start with post
 
-const CONNECTION_URL = env.DB_CONNECTION;
-const PORT = env.PORT || 5000;
+const CONNECTION_URL = process.env.DB_CONNECTION;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
