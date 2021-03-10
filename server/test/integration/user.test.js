9e3dbeb0cec44c2de6a82f6f7d83435f0fff9262
Chris Melendez <chris.melendezg@gmail.com>
2021-03-10 13:41:48 -0600

    Add Create Account tests

--- server/test/integration/user.test.js
+++ server/test/integration/user.test.js
@@ -10,6 +10,50 @@ const getData = (body) => {
   return body.data ? body.data : body;
 };
 
+describe("POST /", () => {
+  let createdUser;
+  it("Successfully created account", async () => {
+    const respCreate = await request.post("/users/").send(testUser);
+    var data = getData(respCreate.body);
+    createdUser = data;
+    // check response
+    expect(respCreate.statusCode).toBe(201);
+    expect(data.id).toBeDefined();
+    expect(data.username).toBe(testUser.username);
+    expect(data.email).toBe(testUser.email);
+    // check if in db
+    const respGet = await request.get(`/users/${createdUser.id}`);
+    data = getData(respGet.body);
+    expect(data.id).toBe(createdUser.id);
+    expect(data.username).toBe(createdUser.username);
+    expect(data.email).toBe(createdUser.email);
+  });
+
+  it("User already exists returns error", async () => {
+    const response = await request.post("/users/").send(testUser);
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe("User already exists");
+  });
+
+  it("Email already in use returns error", async () => {
+    const newUser = {
+      username: "newnew",
+      password: "12345",
+      email: testUser.email,
+    };
+    const response = await request.post("/users/").send(newUser);
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe("Email already in use");
+    await request.delete(`/users/${createdUser.id}`);
+  });
+
+  it("Missing field returns error", async () => {
+    const response = await request.post("/users/").send({ username: "Jest" });
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe("Username, Email or Password missing");
+  });
+});
+
 describe("POST /login", () => {
   let mockUser;
   beforeAll(async () => {
