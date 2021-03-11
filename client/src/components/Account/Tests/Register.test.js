cae8321bd77fb5ca93c08163c576a97fbbad7b95
mjbathtub <mbathie1999@gmail.com>
2021-03-11 00:23:07 -0600

    migrated login and register over to redux mock store

---
+++ client/src/components/Account/Tests/Register.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Register from "../Register";
+
+import { Provider } from "react-redux"; //keeps track of the store which is the global state which allows us to access the store from anywhere within the app.
+import configureStore from "redux-mock-store";
+
+const middleware = [];
+const mockStore = configureStore(middleware);
+const initialState = {
+  account: {
+    loginError: "placeholder state",
+  },
+};
+
+describe("<Register />", () => {
+  it("should render without crashing", () => {
+    shallow(
+      <Provider store={mockStore(initialState)}>
+        <Register />
+      </Provider>
+    );
+  });
+});
