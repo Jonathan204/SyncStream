a30b2a44041b8277bbdae065755939a17145a934
mjbathtub <mbathie1999@gmail.com>
2021-03-10 21:48:59 -0600

    added some initial tests

--- client/src/components/Account/Tests/Account.test.js
+++ client/src/components/Account/Tests/Account.test.js
@@ -1,3 +1,38 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Account from "../Account";
+import Login from "../Login";
+import Register from "../Register";
+
 test("should pass test", () => {
   expect(true).toBeTruthy();
+});
+
+describe("<Account>", () => {
+  let state;
+  beforeEach(() => {
+    state = {
+      activeWindow: "signin",
+    };
+  });
+
+  it("should render without crashing", () => {
+    shallow(<Account />);
+  });
+
+  it("should render <Login /> by default", () => {
+    const wrapper = shallow(<Account />);
+    expect(wrapper.containsMatchingElement(<Login />)).toEqual(true);
+  });
+
+  it("should not render <Register /> by default", () => {
+    const wrapper = shallow(<Account />);
+    expect(wrapper.containsMatchingElement(<Register />)).toEqual(false);
+  });
+
+  // it("should render <Register /> after state change", () => {
+  //   const wrapper = shallow(<Account />);
+  //   test.find("p").simulate("click");
+  //   expect(wrapper.containsMatchingElement(<Register />)).toEqual(true);
+  // });
 });
