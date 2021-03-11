076d899a699212ed93325deaf1d0cacf4ec5bc91
mjbathtub <mbathie1999@gmail.com>
2021-03-10 23:00:00 -0600

    Tests for Account

--- client/src/components/Account/Tests/Account.test.js
+++ client/src/components/Account/Tests/Account.test.js
@@ -1,21 +1,17 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import Account from "../Account";
 import Login from "../Login";
 import Register from "../Register";
 
-test("should pass test", () => {
-  expect(true).toBeTruthy();
-});
+import { Provider } from "react-redux"; //keeps track of the store which is the global state which allows us to access the store from anywhere within the app.
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducers from "../../../reducers";
 
-describe("<Account>", () => {
-  let state;
-  beforeEach(() => {
-    state = {
-      activeWindow: "signin",
-    };
-  });
+const mockStore = createStore(reducers, applyMiddleware(thunk));
 
+describe("<Account />", () => {
   it("should render without crashing", () => {
     shallow(<Account />);
   });
@@ -30,9 +26,25 @@ describe("<Account>", () => {
     expect(wrapper.containsMatchingElement(<Register />)).toEqual(false);
   });
 
-  // it("should render <Register /> after state change", () => {
-  //   const wrapper = shallow(<Account />);
-  //   test.find("p").simulate("click");
-  //   expect(wrapper.containsMatchingElement(<Register />)).toEqual(true);
-  // });
+  it("should render <Register /> after state change", () => {
+    const wrapper = mount(
+      <Provider store={mockStore}>
+        <Account />
+      </Provider>
+    );
+    wrapper.find("p").simulate("click");
+    expect(wrapper.containsMatchingElement(<Register />)).toEqual(true);
+  });
+
+  it("should render <Register /> then render <Login />", () => {
+    const wrapper = mount(
+      <Provider store={mockStore}>
+        <Account />
+      </Provider>
+    );
+    wrapper.find("p").simulate("click");
+    expect(wrapper.containsMatchingElement(<Register />)).toEqual(true);
+    wrapper.find("p").simulate("click");
+    expect(wrapper.containsMatchingElement(<Login />)).toEqual(true);
+  });
 });
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,3 +1,4 @@
 import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import "regenerator-runtime/runtime";
 configure({ adapter: new Adapter() });
