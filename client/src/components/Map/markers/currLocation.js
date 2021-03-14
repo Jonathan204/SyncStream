982c81941684e4484785e5ed38a82639f849fb2c
Chris Melendez <chris.melendezg@gmail.com>
2021-03-13 19:44:01 -0600

    Frontend refactoring for spotify calls

--- client/src/components/Map/markers/currLocation.js
+++ client/src/components/Map/markers/currLocation.js
@@ -1,30 +1,29 @@
-import React from 'react'
-import { PersonCircle } from 'react-bootstrap-icons';
-import UserInfo from '../infoWindow/currLocation.js'
+import React from "react";
+import { PersonCircle } from "react-bootstrap-icons";
+import UserInfo from "../infoWindow/currLocation.js";
 
 class currLocation extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { showInfo: false };
+  constructor(props) {
+    super(props);
+    this.state = { showInfo: false };
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this);
-    }
+  handleClick() {
+    this.setState((state) => ({
+      showInfo: !state.showInfo,
+    }));
+  }
 
-    handleClick() {
-        this.setState(state => ({
-            showInfo: !state.showInfo
-        }));
-    }
-
-    render() {
-        return (
-            <div>
-                <PersonCircle onClick={this.handleClick} color="royalblue" size={50} />
-                {this.state.showInfo && <UserInfo></UserInfo>}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <PersonCircle onClick={this.handleClick} color="royalblue" size={50} />
+        {this.state.showInfo && <UserInfo self={this.props.self}></UserInfo>}
+      </div>
+    );
+  }
 }
 
 export default currLocation;
