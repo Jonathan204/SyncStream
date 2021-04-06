121a0af4c54feb7be9a40b14a6a989c1c7dd60b7
Chris Melendez <chris.melendezg@gmail.com>
2021-04-06 16:19:23 -0500

    start setup for deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
@@ -9,11 +8,14 @@ import userRoutes from "./routes/users.js";
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/users", userRoutes); //every route inside of post routes will start with post
+if (process.env.NODE_ENV === "production")
+  app.use(express.static("../client/build"));
+
+app.use("/api/users", userRoutes); //every route inside of post routes will start with post
 
 const CONNECTION_URL = process.env.DB_CONNECTION;
 const PORT = process.env.PORT || 5000;
